9a1bac19ef2cb7923f6f62e30cecb71788461977
Sarkhan666 <sarkhanak@code.edu.az>
2022-05-10 01:56:21 +0400

    Add project files.

---
+++ Task09.05.2022/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task09._05._2022.Models
+{
+    class Book
+    {
+        public string Code;
+        public string Name;
+        public string AuthorName;
+        public int PageCount;
+        private static int _count;
+
+        static Book()
+        {
+            _count = 0;
+        }
+        public Book(string name, string authorname, int pagecount)
+        {
+            _count++;
+            Name = name;
+            AuthorName = authorname;
+            PageCount = pagecount;
+            Code = $"{Name.Substring(0, 2)}{_count}";
+        }
+    }
+}
